bfb08249ad75affa8d51a5c3eb215f0c40854885
Victor <45390236+Dunkelklinge1@users.noreply.github.com>
2025-06-22 15:55:40 +0200

    Create backup-nextcloud-database.sh

---
+++ backup-nextcloud-database.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+BACKUPMOUNTPOINT="/mnt/backup" # Enty must exist in fstab
+BACKUPTIME="$(date +%F_%H-%M)" # Get current date and time in format YYYY-mm-DD_HH-MM
+BACKUPLABEL="nextcloud-database-$BACKUPTIME"
+DESTINATIONDIR="/mnt/backup/backup-data/mysqldump" # Root dir of all backups
+DBUSER="nextcloud" # DB user
+DBPASSWORD="" # DB password
+DBNAME="nextcloud" # DB name
+
+# Mount backup volume
+mount $BACKUPMOUNTPOINT
+# Check if backup volume is mounted (space after it prevents false positives with mountpoints that contains the same string)
+if grep -qs "$BACKUPMOUNTPOINT " /proc/mounts; then
+    # Create the destination dir
+    mkdir -p $DESTINATIONDIR
+    # Create database dump
+    mariadb-dump --single-transaction -h localhost -u $DBUSER -p$DBPASSWORD $DBNAME > "$DESTINATIONDIR/$BACKUPLABEL.mysqldump"
+    # Delete dumps older than 60 days
+    find "$DESTINATIONDIR" -type f -mtime +60 -name '*.mysqldump' -delete
+
+    echo "Script finished"
+else
+    echo "Failed to mount '$BACKUPMOUNTPOINT'!"
+fi
